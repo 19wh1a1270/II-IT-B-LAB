2f4974e97d2ff7b04ca74761bd7fad025d27299c
19wh1a1270 <69143434+19wh1a1270@users.noreply.github.com>
2020-08-22 00:07:19 +0530

    Add files via upload

---
+++ C++ lab/Experiment 1/increment and decrement operators/pre and post.c
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cin>>a;
+	cout<<"The value of a++ is  "<< a++ << "\n";
+	cout<<"The value of ++a is "<< ++a << "\n";
+	cout<<"The value of --a is "<< --a << "\n";
+	cout<<"The value of a-- is "<< a-- << "\n";
+	return 0;
+}
