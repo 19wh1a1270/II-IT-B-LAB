b8c653ef011b06a626bace3fe9817cefa521bb84
19wh1a1270 <69143434+19wh1a1270@users.noreply.github.com>
2020-08-22 00:03:16 +0530

    Add files via upload

---
+++ C++ lab/Experiment 1/P2(2)/even odd.c
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cin>>a;
+	if(a % 2 == 0)
+	cout<<"The number "<<a<<" is a even number ";
+	else
+	cout<<"The number "<<a<<" is a odd number";
+	return 0;
+}
