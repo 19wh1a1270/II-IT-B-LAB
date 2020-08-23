1d5f77170e769bd6c11afdceeefcdec0dcf40701
19wh1a1270 <69143434+19wh1a1270@users.noreply.github.com>
2020-08-23 19:40:38 +0530

    Add files via upload

---
+++ C++ lab/Experiment 1/P2(1)/num is neg.c
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cin>>a;
+	if(a > 0)
+	cout<<"The number is not negative ";
+	else( a < 0)
+	cout<<a<<"\n";
+	cout<<"The number is negative";
+	return 0;
+}
